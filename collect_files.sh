7098d259ac36a2e3061bfbeb06816cbecb28201f
Sofia <sofia.karinkina@gmail.com>
2025-05-01 12:20:27 +0300

    third

--- collect_files.sh
+++ collect_files.sh
@@ -3,3 +3,14 @@
 if [[ $# -ne 2 ]]; then
     exit 1
 fi
+
+INPUT_DIR="$1"
+OUTPUT_DIR="$2"
+
+if [[ ! -d "$INPUT_DIR" ]]; then
+    exit 1
+fi
+
+mkdir -p "$OUTPUT_DIR"
+
+find "$INPUT_DIR" -type f -exec cp {} "$OUTPUT_DIR" \;
